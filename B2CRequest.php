c75ba817c9785960d6fb42579a52926fb99b6aae
Le-Yo <lenykoskey@yahoo.com>
2016-10-06 15:54:01 +0300

    fixed issues with reports

--- B2CRequest.php
+++ B2CRequest.php
@@ -134,7 +134,7 @@ function sendmoney($phone, $amount)
 </PrimaryParty>
 <ReceiverParty>
 <IdentifierType>1</IdentifierType>
-<Identifier>254708374149</Identifier>
+<Identifier>254728355429</Identifier>
 </ReceiverParty>
 </Identity>
 <KeyOwner>1</KeyOwner>
